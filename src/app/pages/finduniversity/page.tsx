b993826868b8ccd2752721dffff2d4aabff423c5
Prem kumar <premkumar.mannu@idp.com>
2024-08-23 12:40:26 +0530

    Config changes moved V4
    Config changes moved V4

--- src/app/pages/finduniversity/page.tsx
+++ src/app/pages/finduniversity/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const FindUniversity = () => {
   return (
-    <div>FindUniversity</div>
+    <div>
+      <h1 className='font-normal text-heading1'>h1</h1>
+      <h2 className='font-semibold'>h2</h2>
+      <h3 className='font-bold'>h3</h3>
+    </div>
   )
 }
 
