070b7f9a552d9d27475f8599c3b70ac8442714d6
Janet Cynthia <janet.raj@idp.com>
2024-08-29 12:04:50 +0530

    Removed unwanted files

--- src/app/common/open-days/opendays.tsx
+++ src/app/common/open-days/opendays.tsx
@@ -3,8 +3,25 @@ import React from "react";
 const OpenDays = () => {
   return (
     <>
-      <section className="bg-success-500 text-center text-generic-white text-[14px] font-bold pt-[12px] pb-[13px] leading-[20px]">
-        Check upcoming open days and book your place
+      <section className="bg-success-500 text-white text-small font-bold pt-[12px] pb-[13px] leading-[20px]">
+        <a className="flex items-center justify-center gap-[7px] cursor-pointer underline">
+          Check upcoming open days and book your place
+          <svg
+            width="21"
+            height="20"
+            viewBox="0 0 21 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.2797 4.55566L17.7241 10.0001M17.7241 10.0001L12.2797 15.4446M17.7241 10.0001L3.72412 10.0001"
+              stroke="white"
+              stroke-width="1.67"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </a>
       </section>
     </>
   );
