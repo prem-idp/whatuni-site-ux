c2e206c71ef4a71de56443d36eec8c3e69d63483
prem-idp <premkumar.mannu@idp.com>
2024-10-08 14:40:11 +0530

    created the new here component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,6 +44,8 @@ const config: Config = {
     },
     extend: {
       colors: {
+        "success-400": "#34D399",
+        "blue-200": "#0FBEFD",
         grey: {
           300: "#333",
           400: "#707070",
@@ -113,7 +115,9 @@ const config: Config = {
         "custom-2": "0px 1px 2px 0px rgba(16,24,40,0.05)",
         "custom-3": "0px 1px 2px -1px rgba(16, 24, 40, 0.1), 0px 1px 3px 0px rgba(16, 24, 40, 0.1)",
         "custom-4": "0px 2px 4px -2px rgba(16, 24, 40, 0.1),0px -2px 6px -1px rgba(16,24,40,0.07)",
+        "custom-5": "0px 4px 6px -4px rgba(16,24,40,0.1),0px 10px 15px -3px rgba(16,24,40,0.1)",
       },
+      
     },
   },
   plugins: [],
