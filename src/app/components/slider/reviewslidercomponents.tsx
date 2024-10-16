1c0cb0afc2f159aeeec49b006736e0e86ca738ee
prem-idp <premkumar.mannu@idp.com>
2024-10-16 12:55:01 +0530

    All changes merged from stash & develop on 16-10-2024
    All changes merged from stash & develop on 16-10-2024

--- src/app/components/slider/reviewslidercomponents.tsx
+++ src/app/components/slider/reviewslidercomponents.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 // import Slider from "react-slick";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
-import Reviewscard from '../cards/reviewscard';
+import Reviewscard from '../cards/reviews-card/reviewscard';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
