acdf62c774ec7321b928748d93b58dfc30888e43
Prem kumar <premkumar.mannu@idp.com>
2024-10-18 11:24:44 +0000

    Update discoverslidercomponents.tsx

--- src/app/components/slider/discoverslidercomponents.tsx
+++ src/app/components/slider/discoverslidercomponents.tsx
@@ -1,61 +1,207 @@
 "use client";
 
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import {FreeMode, Navigation, Pagination} from 'swiper/modules';
+import Discovercard from '../cards/discover/discovercard';
 
-import Reviewscard from '../cards/reviews-card/reviewscard';
+const Discoverslidercomponents1 = () => {
 
-const Reviewslidercomponents = () => {
+// Toggle Menu
+const [isMobile, setIsMobile] = useState(false);
+const [isOpen, setIsOpen] = useState(false);
+const mobileToggleOpen = () => {
+  setIsOpen(!isOpen);
+};
+
+useEffect(() => {
+const handleResize = () => {
+  setIsMobile(window.innerWidth <= 991);
+};
+handleResize();
+window.addEventListener('resize', handleResize);
+return () => window.removeEventListener('resize', handleResize);
+}, []);    
+  
   return (
     <> 
+    
+    {isMobile ? (
     <div className='slider-container'>
-      <Swiper
-        pagination={true}
-        navigation= {true}
-        breakpoints={{
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 8,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 16,
-          },
-          1200: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-        }}
-        modules={[FreeMode, Pagination, Navigation]}
-        className="MultiSwiper">
-              <SwiperSlide>
-                  <Reviewscard />
-              </SwiperSlide>
-              <SwiperSlide>
-                  <Reviewscard />
-              </SwiperSlide>
-              <SwiperSlide>
-                  <Reviewscard />
-              </SwiperSlide>
-              <SwiperSlide>
-                  <Reviewscard />
-              </SwiperSlide>
-              <SwiperSlide>
-                  <Reviewscard />
-              </SwiperSlide>
-              <SwiperSlide>
-                  <Reviewscard />
-              </SwiperSlide>
-            </Swiper>   
-          </div>
+   <Swiper
+   pagination={true}
+   breakpoints={{
+     320: {
+       slidesPerView: 1,
+       spaceBetween: 8,
+     },
+     768: {
+       slidesPerView: 2,
+       spaceBetween: 16,
+     },
+   }}
+   modules={[FreeMode, Pagination]}
+   className="MultiSwiper">
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-primary-100 rounded-[8px] overflow-hidden'>
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-primary-500 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>courses</div>
+                       <h5 className='font-bold'>Looking for courses?</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image1x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>                                                  
+       </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-secondary-200 rounded-[8px] overflow-hidden'>    
+                   <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-positive-default bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>universities</div>
+                       <h5 className='font-bold'>Pick your perfect uni</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-tertiary-100 rounded-[8px] overflow-hidden'> 
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-tertiary-600 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>career</div>
+                       <h5 className='font-bold'>Take our careers quiz</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image1x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-primary-100 rounded-[8px] overflow-hidden'>    
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-primary-500 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>Subject guides</div>
+                       <h5 className='font-bold'>Find out what to study</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-secondary-200 rounded-[8px] overflow-hidden'>    
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-positive-default bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>open days</div>
+                       <h5 className='font-bold'>Find an open day</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image3x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-tertiary-100 rounded-[8px] overflow-hidden'>    
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-tertiary-600 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>app</div>
+                       <h5 className='font-bold'>Download the app</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+       </Swiper>
+       </div>
+    ) : (
+            <div className='discover grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-[20px]'>
+              <div className="discover-card">
+                <Link href="#" className='block bg-primary-100 rounded-[8px] overflow-hidden'>
+                    <div className='discover-card flex justify-between gap-[8px]'>
+                        <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                            <div className='w-fit uppercase font-bold x-small text-primary-500 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>courses</div>
+                            <h5 className='font-bold'>Looking for courses?</h5>
+                        </div>
+                        <Image src="/assets/images/discover/discover-feature-image1x3x.png" width="186" height="200"  alt='discover'/>
+                    </div>
+                </Link>                                                  
+                </div>
+              <div className="discover-card">
+                <Link href="#" className='block bg-secondary-200 rounded-[8px] overflow-hidden'>    
+                        <div className='discover-card flex justify-between gap-[8px]'>
+                        <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                            <div className='w-fit uppercase font-bold x-small text-positive-default bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>universities</div>
+                            <h5 className='font-bold'>Pick your perfect uni</h5>
+                        </div>
+                        <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+                    </div>
+                </Link>
+                </div>
+              <div className="discover-card">
+                <Link href="#" className='block bg-tertiary-100 rounded-[8px] overflow-hidden'> 
+                    <div className='discover-card flex justify-between gap-[8px]'>
+                        <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                            <div className='w-fit uppercase font-bold x-small text-tertiary-600 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>career</div>
+                            <h5 className='font-bold'>Take our careers quiz</h5>
+                        </div>
+                        <Image src="/assets/images/discover/discover-feature-image1x3x.png" width="186" height="200"  alt='discover'/>
+                    </div>
+                </Link>
+                </div>
+              <div className="discover-card">
+                <Link href="#" className='block bg-primary-100 rounded-[8px] overflow-hidden'>    
+                    <div className='discover-card flex justify-between gap-[8px]'>
+                        <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                            <div className='w-fit uppercase font-bold x-small text-primary-500 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>Subject guides</div>
+                            <h5 className='font-bold'>Find out what to study</h5>
+                        </div>
+                        <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+                    </div>
+                </Link>
+                </div>
+              <div className="discover-card">
+                <Link href="#" className='block bg-secondary-200 rounded-[8px] overflow-hidden'>    
+                    <div className='discover-card flex justify-between gap-[8px]'>
+                        <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                            <div className='w-fit uppercase font-bold x-small text-positive-default bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>open days</div>
+                            <h5 className='font-bold'>Find an open day</h5>
+                        </div>
+                        <Image src="/assets/images/discover/discover-feature-image3x3x.png" width="186" height="200"  alt='discover'/>
+                    </div>
+                </Link>
+                </div>
+              <div className="discover-card">
+                <Link href="#" className='block bg-tertiary-100 rounded-[8px] overflow-hidden'>    
+                    <div className='discover-card flex justify-between gap-[8px]'>
+                        <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                            <div className='w-fit uppercase font-bold x-small text-tertiary-600 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>app</div>
+                            <h5 className='font-bold'>Download the app</h5>
+                        </div>
+                        <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+                    </div>
+                </Link>
+                </div>
+            </div>
+    )}
+       
+          
     </>
   );
 };
 
-export default Reviewslidercomponents;
+export default Discoverslidercomponents1;
