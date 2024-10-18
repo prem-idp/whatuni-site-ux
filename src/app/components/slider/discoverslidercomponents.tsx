5a6d00399c2365cf78c1669725272126882c0f84
Prem kumar <premkumar.mannu@idp.com>
2024-10-18 11:22:44 +0000

    Update discoverslidercomponents.tsx

--- src/app/components/slider/discoverslidercomponents.tsx
+++ src/app/components/slider/discoverslidercomponents.tsx
@@ -1,15 +1,136 @@
 "use client";
+
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import {FreeMode, Navigation, Pagination} from 'swiper/modules';
 import Discovercard from '../cards/discover/discovercard';
 
-const Discoverslidercomponents = () => {
+const Discoverslidercomponents1 = () => {
+
+// Toggle Menu
+const [isMobile, setIsMobile] = useState(false);
+const [isOpen, setIsOpen] = useState(false);
+const mobileToggleOpen = () => {
+  setIsOpen(!isOpen);
+};
+
+useEffect(() => {
+const handleResize = () => {
+  setIsMobile(window.innerWidth <= 991);
+};
+handleResize();
+window.addEventListener('resize', handleResize);
+return () => window.removeEventListener('resize', handleResize);
+}, []);    
+  
   return (
     <> 
-        <div className="slider-container">
-            <div>
-            <div className="discover-card">
+    
+    {isMobile ? (
+    <div className='slider-container'>
+   <Swiper
+   pagination={true}
+   breakpoints={{
+     320: {
+       slidesPerView: 1,
+       spaceBetween: 8,
+     },
+     768: {
+       slidesPerView: 2,
+       spaceBetween: 16,
+     },
+   }}
+   modules={[FreeMode, Pagination]}
+   className="MultiSwiper">
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-primary-100 rounded-[8px] overflow-hidden'>
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-primary-500 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>courses</div>
+                       <h5 className='font-bold'>Looking for courses?</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image1x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>                                                  
+       </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-secondary-200 rounded-[8px] overflow-hidden'>    
+                   <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-positive-default bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>universities</div>
+                       <h5 className='font-bold'>Pick your perfect uni</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-tertiary-100 rounded-[8px] overflow-hidden'> 
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-tertiary-600 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>career</div>
+                       <h5 className='font-bold'>Take our careers quiz</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image1x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-primary-100 rounded-[8px] overflow-hidden'>    
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-primary-500 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>Subject guides</div>
+                       <h5 className='font-bold'>Find out what to study</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-secondary-200 rounded-[8px] overflow-hidden'>    
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-positive-default bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>open days</div>
+                       <h5 className='font-bold'>Find an open day</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image3x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+         <SwiperSlide>
+         <div className="discover-card">
+           <Link href="#" className='block bg-tertiary-100 rounded-[8px] overflow-hidden'>    
+               <div className='discover-card flex justify-between gap-[8px]'>
+                   <div className="flex flex-col justify-between p-[20px] pr-[0]">
+                       <div className='w-fit uppercase font-bold x-small text-tertiary-600 bg-white/[.6] px-[6px] py-[2px] rounded-[4px]'>app</div>
+                       <h5 className='font-bold'>Download the app</h5>
+                   </div>
+                   <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
+               </div>
+           </Link>
+           </div>
+         </SwiperSlide>
+       </Swiper>
+       </div>
+    ) : (
+            <div className='discover grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-[20px]'>
+              <div className="discover-card">
                 <Link href="#" className='block bg-primary-100 rounded-[8px] overflow-hidden'>
                     <div className='discover-card flex justify-between gap-[8px]'>
                         <div className="flex flex-col justify-between p-[20px] pr-[0]">
@@ -19,10 +140,8 @@ const Discoverslidercomponents = () => {
                         <Image src="/assets/images/discover/discover-feature-image1x3x.png" width="186" height="200"  alt='discover'/>
                     </div>
                 </Link>                                                  
-            </div>
-            </div>
-            <div>
-                <div className="discover-card">
+                </div>
+              <div className="discover-card">
                 <Link href="#" className='block bg-secondary-200 rounded-[8px] overflow-hidden'>    
                         <div className='discover-card flex justify-between gap-[8px]'>
                         <div className="flex flex-col justify-between p-[20px] pr-[0]">
@@ -33,9 +152,7 @@ const Discoverslidercomponents = () => {
                     </div>
                 </Link>
                 </div>
-            </div>
-            <div>
-                <div className="discover-card">
+              <div className="discover-card">
                 <Link href="#" className='block bg-tertiary-100 rounded-[8px] overflow-hidden'> 
                     <div className='discover-card flex justify-between gap-[8px]'>
                         <div className="flex flex-col justify-between p-[20px] pr-[0]">
@@ -46,9 +163,7 @@ const Discoverslidercomponents = () => {
                     </div>
                 </Link>
                 </div>
-            </div>
-            <div>
-                <div className="discover-card">
+              <div className="discover-card">
                 <Link href="#" className='block bg-primary-100 rounded-[8px] overflow-hidden'>    
                     <div className='discover-card flex justify-between gap-[8px]'>
                         <div className="flex flex-col justify-between p-[20px] pr-[0]">
@@ -59,9 +174,7 @@ const Discoverslidercomponents = () => {
                     </div>
                 </Link>
                 </div>
-            </div>
-            <div>
-                <div className="discover-card">
+              <div className="discover-card">
                 <Link href="#" className='block bg-secondary-200 rounded-[8px] overflow-hidden'>    
                     <div className='discover-card flex justify-between gap-[8px]'>
                         <div className="flex flex-col justify-between p-[20px] pr-[0]">
@@ -72,9 +185,7 @@ const Discoverslidercomponents = () => {
                     </div>
                 </Link>
                 </div>
-            </div>
-            <div>
-                <div className="discover-card">
+              <div className="discover-card">
                 <Link href="#" className='block bg-tertiary-100 rounded-[8px] overflow-hidden'>    
                     <div className='discover-card flex justify-between gap-[8px]'>
                         <div className="flex flex-col justify-between p-[20px] pr-[0]">
@@ -84,11 +195,13 @@ const Discoverslidercomponents = () => {
                         <Image src="/assets/images/discover/discover-feature-image2x3x.png" width="186" height="200"  alt='discover'/>
                     </div>
                 </Link>
-                </div> 
+                </div>
             </div>
-          </div>
+    )}
+       
+          
     </>
   );
 };
 
-export default Discoverslidercomponents;
+export default Discoverslidercomponents1;
