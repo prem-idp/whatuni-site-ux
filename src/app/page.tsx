e495e47c3003aef6f934e0b92d5590cbb1e14ba6
prem-idp <premkumar.mannu@idp.com>
2024-10-22 18:03:56 +0530

    Latest changes
    Latest changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ import Testimonialcomponents from "./home/testimonials/testimonialcomponents";
 import Reviewscomponents from "./home/reviews/reviewscomponents";
 import Advicecomponents from "./home/advice/advicecomponents";
 import OurPartnerComponent from "./components/our-partners/ourpartnercomponent";
-import UAParser from "ua-parser-js";
+// import UAParser from "ua-parser-js";
 import { headers } from "next/headers";
 import Tagcloudcomponents from "./home/tag-cloud/tagcloudcomponents";
 
 export default function Home() {
 
-  const { get } = headers()
-  const ua = get('user-agent')
+  // const { get } = headers()
+  // const ua = get('user-agent')
 
-  const device = new UAParser(ua || '').getDevice();
-  const isMobile = device.type
+  // const device = new UAParser(ua || '').getDevice();
+  // const isMobile = device.type
     
   return (
     <>
