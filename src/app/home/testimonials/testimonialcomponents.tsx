a0d6f4b1d6732365464cbc51d700080232940b82
prem-idp <premkumar.mannu@idp.com>
2024-11-19 17:55:01 +0530

    Latest testimonial changes moved
    Latest testimonial changes moved

--- src/app/home/testimonials/testimonialcomponents.tsx
+++ src/app/home/testimonials/testimonialcomponents.tsx
@@ -11,10 +11,17 @@ const Testimonialcomponents = () => {
                         <p className='font-normal small mt-[8px]'>Subheading</p>
                     </div>
                     <div className='testimonials-inner-wrap grid grid-cols-1 lg:grid-cols-2 gap-[16px]'>
-                        <div className='video-container flex justify-center items-center rounded-[8px] overflow-hidden relative'>
-                            <img className='block w-full'  src='/assets/images/testimonials/video_thumb_img.jpg' alt="video thumbnail image" width={600} height={316} />
-                            <div className='video-play-icon absolute cursor-pointer'>
-                                <Image src="/assets/icons/video_play_icon.svg" width="52" height="52" alt="" />                                                           
+                        
+                        <div className='video-container flex justify-center items-center lg:items-start xl:items-center rounded-[8px] overflow-hidden relative'>
+                            <div className='video w-full '>
+                                <video className="w-full" src="https://videos.ctfassets.net/szez98lehkfm/55B87nXDB6elXrfpVzAN2f/cb43e059d141307aed6dd617a202a86f/wuscas_2024_video.mp4" width="600" height="316"  controls
+                                ></video>
+                            </div>
+                            <div className='block'>
+                                <img className='block w-full'  src='/assets/images/testimonials/video_thumb_img.jpg' alt="video thumbnail image" width={600} height={316} />
+                                <div className='video-play-icon absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] cursor-pointer'>
+                                    <Image src="/assets/icons/video_play_icon.svg" width="52" height="52" alt="" />                                                           
+                                </div>
                             </div>
                         </div>
                         <div className='testimonial-card grid content-between p-[24px] rounded-[8px] gap-[16px] bg-primary-500'>
