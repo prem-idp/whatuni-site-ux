930583c9490cbb82f8702539f92aaa1351758984
prem-idp <premkumar.mannu@idp.com>
2024-12-03 11:45:47 +0530

    COLS Results changes moved
    COLS Results changes moved

--- src/app/components/faq/faqcomponents.tsx
+++ src/app/components/faq/faqcomponents.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 import Link from 'next/link'
 
 const Faqcomponents = () => {
+    const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+    const toggleAccordion = (index: number) => {
+      setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
+    };
+
+    const faq_items = [
+        { 
+            title: "How can I see my existing clients through Designership?", 
+            content: "It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth." 
+        },
+        { 
+            title: "How does Designership attract clients?", 
+            content: "It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth." 
+        },
+        { 
+            title: "What type of support does Designership provide?", 
+            content: "It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth." 
+        },
+        { 
+            title: "How can I see my existing clients through Designership?", 
+            content: "It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth." 
+        },
+        { 
+            title: "How does Designership attract clients?", 
+            content: "It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth." 
+        },
+        { 
+            title: "What type of support does Designership provide?", 
+            content: "It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth." 
+        },
+      ];
   return (
     <div className="reviews-container bg-white">
       <div className="max-w-container mx-auto">
@@ -11,50 +44,20 @@ const Faqcomponents = () => {
                     <p className="font-normal small mt-[8px]">Subheading</p>
                 </div>
                 <div className="accordion flex flex-col rounded-[6px] border border-grey-200 px-[16px]">
-                    <div className="accordion-item flex flex-col gap-[8px] py-[16px] border-b border-b-grey-200 last:border-b-0 cursor-pointer">
-                        <div className='accordion-header flex items-center justify-between gap-[48px] para font-semibold text-grey300'>
-                            How can I see my existing clients through Designership?
+                {faq_items.map((item,index) => (
+                    <div className="accordion-item flex flex-col gap-[8px] py-[16px] border-b border-b-grey-200 last:border-b-0 cursor-pointer" key={index}>
+                        <div className='accordion-header flex items-center justify-between gap-[48px] para font-semibold text-grey300' onClick={() => toggleAccordion(index)}>
+                            {item.title}
                             <span>
-                                <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1 7.5L7 1.5L13 7.5" stroke="#82898F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
+                            <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 1L6 6L1 1" stroke="#82898F" strokeWidth="1.67" strokeLinecap="round" strokeLinejoin="round"/>
+                            </svg>
                             </span>
                         </div>
-                        <div className="accordion-body hidden">It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth.</div>                                                    
+                        <div className={`accordion-body  ${openIndex === index ? 'expanded' : 'collapsed'}`}>{item.content}</div>
+                                                  
                     </div>
-                    <div className="accordion-item flex flex-col gap-[8px] py-[16px] border-b border-b-grey-200 last:border-b-0">
-                    <div className='accordion-header flex items-center justify-between gap-[48px] para font-semibold text-grey300'>
-                            How can I see my existing clients through Designership?
-                            <span>
-                                <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1 7.5L7 1.5L13 7.5" stroke="#82898F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
-                            </span>
-                        </div>
-                        <div className="accordion-body hidden">It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth.</div>                                                    
-                    </div>
-                    <div className="accordion-item flex flex-col gap-[8px] py-[16px] border-b border-b-grey-200 last:border-b-0">
-                    <div className='accordion-header flex items-center justify-between gap-[48px] para font-semibold text-grey300'>
-                             How does Designership attract clients?
-                            <span>
-                                <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1 7.5L7 1.5L13 7.5" stroke="#82898F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
-                            </span>
-                        </div>
-                        <div className="accordion-body">It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth.</div>                                                    
-                    </div>
-                    <div className="accordion-item flex flex-col gap-[8px] py-[16px] border-b border-b-grey-200 last:border-b-0">
-                    <div className='accordion-header flex items-center justify-between gap-[48px] para font-semibold text-grey300'>
-                            What type of support does Designership provide?
-                            <span>
-                                <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1 7.5L7 1.5L13 7.5" stroke="#82898F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
-                            </span>
-                        </div>
-                        <div className="accordion-body hidden">It’s incredibly important to us that you’re supported. Designership has a dedicated team to provide answers, advice, and context throughout your experience with Designership. Your feedback and input is a huge part of your growth.</div>                                                    
-                    </div>
+                ))}
                 </div>
             </div>
         </div>
