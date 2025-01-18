c0b7cf97a97df4918edf2a07d0218fdc6fe47877
prem-idp <premkumar.mannu@idp.com>
2025-01-19 01:38:31 +0530

    Framer animation changes moved
    Framer animation changes moved

--- src/app/components/registeration/registerationcomponents.tsx
+++ src/app/components/registeration/registerationcomponents.tsx
@@ -6,6 +6,7 @@ import Signincomponents from './signincomponents'
 import Signupcomponents from './signupcomponents'
 import Signedupcomponents from './signedupcomponents'
 import Socialsignincomponent from './socialsignincomponent'
+import { AnimatePresence, motion } from "motion/react"
 
 const Registerationcomponents = () => {
     const [isOpen,setIsOpen] = useState(true);
@@ -22,19 +23,26 @@ const Registerationcomponents = () => {
     }, [])
   return (
     <div className='modal modal-container flex  justify-center items-start md:items-center backdrop-shadow-white fixed top-0 right-0 left-0 bottom-0 bg-white'>
-    <div className='modal-box shadow-custom-6 w-full md:w-[728px] bg-white md:rounded-[8px] overflow-hidden relative'>
-        <div className='modal_close absolute top-[12px] right-[12px] z-[1] cursor-pointer'>
-            <Image className='block' src="/assets/icons/modal_close.svg" width="12" height="12"  alt='modal close'/>
-        </div>
-        {/* <Signupcomponents /> */}
-        {/* <Signedupcomponents /> */}
-         {isVisible ? (
-            <Socialsignincomponent $showSignupComp={showSignupComp} />
-        ):(
-            <Signupcomponents />
-        )}
-        {/* <Signincomponents /> */}
-    </div>
+      <AnimatePresence>
+      <motion.div
+        initial={{y: 30,opacity:0}}
+        animate={{y:0,opacity:1}}
+        exit = {{y: 30,opacity:0}} 
+        transition= {{duration: 0.25, ease: "easeInOut"}} 
+      className='modal-box shadow-custom-6 w-full md:w-[728px] bg-white md:rounded-[8px] overflow-hidden relative'>
+          <div className='modal_close absolute top-[12px] right-[12px] z-[1] cursor-pointer'>
+              <Image className='block' src="/assets/icons/modal_close.svg" width="12" height="12"  alt='modal close'/>
+          </div>
+          {/* <Signupcomponents /> */}
+          {/* <Signedupcomponents /> */}
+          {isVisible ? (
+              <Socialsignincomponent $showSignupComp={showSignupComp} />
+          ):(
+              <Signupcomponents />
+          )}
+          {/* <Signincomponents /> */}
+      </motion.div>
+      </AnimatePresence>
     </div>  
   )
 }
