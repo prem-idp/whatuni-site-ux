040dd63e8082e5c968734b3046f534bf5b831f0d
prem-idp <premkumar.mannu@idp.com>
2025-02-12 01:22:21 +0530

    Latest CD review lightbox changes moved
    Latest CD review lightbox changes moved

---
+++ src/app/components/slider/reviewthumbgalleryslidercomponents.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState, useRef, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Thumbs } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+const Reviewthumbgalleryslidercomponents = () => {
+    const [isMobileView, setIsMobile] = useState(false);
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  
+    const images = [
+        "/assets/images/course-details/Frame 9350.jpg",
+        "/assets/images/course-details/Frame 9351.jpg",
+        "/assets/images/course-details/Frame 9352.jpg",
+        "/assets/images/course-details/Frame 9353.jpg",
+        "/assets/images/course-details/Frame 9354.jpg",
+        "/assets/images/course-details/Frame 9355.jpg",
+    ];
+    
+      useEffect(() => {
+        let isMobile = window.innerWidth >= 767;
+        setIsMobile(isMobile);
+        console.log(isMobile);
+      }, []);
+
+  return (
+    <>
+     <div className="max-w-lg mx-auto">
+      {/* Main Swiper */}
+            <Swiper
+                spaceBetween={8}
+                navigation={true}
+                thumbs={{ swiper: thumbsSwiper }}
+                modules={[Navigation, Thumbs]}
+                className="reviewMainSwiper MultiSwiper"
+            >
+                {images.map((src, index) => (
+                <SwiperSlide key={index}>
+                    <img src={src} alt={`Slide ${index}`} className="w-full rounded-lg" />
+                </SwiperSlide>
+                ))}
+            </Swiper>
+      {/* Thumbnail Swiper */}
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={8}
+        freeMode={true}
+        watchSlidesProgress={true}
+        breakpoints={{
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 8,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 10,          },
+          }}
+        modules={[Thumbs]}
+        className="thumbSwiper"
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index} className="cursor-pointer">
+            <img src={src} alt={`Thumbnail ${index}`} className="w-full h-auto object-cover rounded-[4px]" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+</>
+  )
+}
+
+export default Reviewthumbgalleryslidercomponents
