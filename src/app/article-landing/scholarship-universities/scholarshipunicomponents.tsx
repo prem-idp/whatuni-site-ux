f733e1031be33d0fc8f501c98c874d8c9432e87d
prem-idp <premkumar.mannu@idp.com>
2025-02-17 17:04:56 +0530

    Clearing CD changes moved
    Clearing CD changes moved

--- src/app/article-landing/scholarship-universities/scholarshipunicomponents.tsx
+++ src/app/article-landing/scholarship-universities/scholarshipunicomponents.tsx
@@ -4,20 +4,20 @@ import React from 'react'
 const Scholarshipunicomponents = () => {
   return (
     <div className='scholarship-container bg-white'> 
-    <div className="max-w-container mx-auto">
-        <div className='scholarship-card-container px-[0] py-[34px] md:py-[64px]'>
-            <div className='scholarship-header  flex flex-col gap-[4px] px-[16px] md:px-[20px] xl:px-[0] mb-[26px] md:mb-[32px]'>
-                <div className="h2 font-bold">Heading</div>
-                <p className='font-normal small'>Subheading</p>
-            </div>
-            <div className='scholarship-course-container '>
-              <div className="scholarship-inner-wrap"> 
-                <Scholarshipunislidercomponents />
+      <div className="max-w-container mx-auto">
+          <div className='scholarship-card-container px-[0] py-[34px] md:py-[64px]'>
+              <div className='scholarship-header  flex flex-col gap-[4px] px-[16px] md:px-[20px] xl:px-[0] mb-[26px] md:mb-[32px]'>
+                  <div className="h2 font-bold">Heading</div>
+                  <p className='font-normal small'>Subheading</p>
+              </div>
+              <div className='scholarship-course-container '>
+                <div className="scholarship-inner-wrap"> 
+                  <Scholarshipunislidercomponents />
+                </div>
               </div>
             </div>
-          </div>
+      </div>
     </div>
-  </div>
   )
 }
 
