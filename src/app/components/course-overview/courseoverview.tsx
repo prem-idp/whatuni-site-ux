e87cabbaea1c0519d93596682fbf18a16b863eb2
Ramkumar M <ramkumar.manokaran@idp.com>
2025-03-14 17:45:49 +0530

    commented

---
+++ src/app/components/course-overview/courseoverview.tsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import Image from 'next/image';
+
+// const Courseoverview = ({ data } : { data: any}) => {
+//   return (
+//       <div key={data.title} className="">
+//           <div>
+//           <div className="h5">{data.title}</div>
+//           <span>{data.description}</span>
+//           </div>          
+//           <p>{data.details}</p>
+//           <div className="">
+//               <div className="">
+                  
+                
+//                   <div className="">{data.cardTitle}</div>
+//                   <span className="">{data.cardDetails}</span>
+//               </div>
+//           </div>
+//     </div>
+//   )
+// }
+
+// export default Courseoverview;
