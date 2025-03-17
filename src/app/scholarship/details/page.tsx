ec6af70135e934824134f201ba9a0110bf0e93af
Ramkumar M <ramkumar.manokaran@idp.com>
2025-03-17 14:07:19 +0530

    updates

--- src/app/scholarship/details/page.tsx
+++ src/app/scholarship/details/page.tsx
@@ -1,46 +1,47 @@
 // import Courseoverview from '@/app/components/course-overview/courseoverview';
-import React from 'react'
+// import React from 'react'
 
 
-const courseoverview = () => {
-  const selectionProcess = [
-    {
-      "title":"Selection process section",
-      "description":"What is the selection process for the [Scholarship name]? ",
-      "details": "Here’s an outline of the selection criteria and process to help you understand how the recipients are chosen: ",
-      "cardGrid": [
-        {
-          "cardTitle": "Selection basis",
-          "cardDetails": "Academic Excellence, Financial Need, etc.",
-          "cardIcon": "",
-          "cardImgname": "",
-        },        
-        {
-          "cardTitle": "Selection basis",
-          "cardDetails": "Academic Excellence, Financial Need, etc.",
-          "cardIcon": "",
-          "cardImgname": "",
-        },        
-        {
-          "cardTitle": "Selection basis",
-          "cardDetails": "Academic Excellence, Financial Need, etc.",
-          "cardIcon": "",
-          "cardImgname": "",
-        },        
-      ]
+// const page = () => {
+//   const courseData  = [
+//     {
+//       "title":"Selection process section",
+//       "description":"What is the selection process for the [Scholarship name]? ",
+//       "details": "Here’s an outline of the selection criteria and process to help you understand how the recipients are chosen: ",
+//       "cardGrid": [
+//         {
+//           "cardTitle": "Selection basis",
+//           "cardDetails": "Academic Excellence, Financial Need, etc.",
+//           "cardIcon": "",
+//           "cardImgname": "",
+//         },        
+//         {
+//           "cardTitle": "Selection basis",
+//           "cardDetails": "Academic Excellence, Financial Need, etc.",
+//           "cardIcon": "",
+//           "cardImgname": "",
+//         },        
+//         {
+//           "cardTitle": "Selection basis",
+//           "cardDetails": "Academic Excellence, Financial Need, etc.",
+//           "cardIcon": "",
+//           "cardImgname": "",
+//         },        
+//       ]
 
-    }
-]
-  return (
-      <>
-      <section>
-        <p>test</p>
-        <div className="max-w-container">
-          {/* <Courseoverview data={ selectionProcess } />           */}
-        </div>
-         </section>
-    </>
-  )
-}
+//     }
+// ]
+//   return (
+//       <>
+//       <section>
+//         <p>test</p>
+//         <div className="max-w-container">
+          
+//           <Courseoverview data={courseData } />
+//         </div>
+//          </section>
+//     </>
+//   )
+// }
 
-export default courseoverview;
+// export default page;
